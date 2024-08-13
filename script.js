797703c74db06f39e28f9e00f2dc7fa027aa2910
Teexy360 <klebersomoliveira90@gmail.com>
2024-08-12 23:47:23 -0300

    login-page

---
+++ script.js
@@ -0,0 +1,16 @@
+function login(){
+  let log = document.querySelector('.container-signup')
+  log.style.display = 'none'
+
+  let sign = document.querySelector('.container-login')
+  sign.style.display = 'block'
+}
+
+
+function signup(){
+  let log = document.querySelector('.container-signup')
+  log.style.display = 'block'
+
+  let sign = document.querySelector('.container-login')
+  sign.style.display = 'none'
+}
